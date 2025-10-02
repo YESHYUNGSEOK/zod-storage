b2fdd50c6fc41e3bae91a60cda7467f5eb16fe77
NohHyungseok <hyungseok.noh@partridgesystems.com>
2025-10-02 17:23:57 +0900

    build: export ZsConfig

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { zs } from './core/zs';
 export { zodStorage } from './core/zodStorage';
 
+export type { ZsConfig } from './core/zs';
 export type { SafeStorage, SafeStorageGetOptions, StorageType } from './types/type';
